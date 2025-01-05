8f3d79b6136120a9a7d4a192b1aff9ae2623ed9b
KowsikaJ <kowsikaj05@gmail.com>
2025-01-05 08:47:27 +0530

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 let productsHTML = '';
-import {cart} from '../data/cart.js';
-import {products} from '../data.products.js';
+import {cart,addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 products.forEach((product) => {
     productsHTML += `
@@ -58,41 +58,30 @@ products.forEach((product) => {
 
 console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML=productsHTML;
-const addedMessageTimeouts={};
-document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-        button.addEventListener('click',()=>{
-            const {productId}= button.dataset;
-            
-            let matchingItem;
-            cart.forEach((item)=>{
-                if(productId===item.productId){
-                    matchingItem=item;
-                }
-            });
-            const quantitySelector = document.querySelector(
-                `.js-quantity-selector-${productId}`
-            );
-            const quantity = Number(quantitySelector.value);
-            if(matchingItem){
-                matchingItem.quantity+=quantity;
-            }
-            else{
-                cart.push({
-                    productId,
-                    quantity
-                });
 
-            }
-            let cartQuantity=0;
-            cart.forEach((item)=>{
-                cartQuantity+=item.quantity;
+
+function updateCartQuantity(){
+    let cartQuantity=0;
+            cart.forEach((cartItem)=>{
+                cartQuantity+=cartItem.quantity;
             });
 
             document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
 
-            const addedMessage=document.querySelector(
+}
+
+document.querySelector('.js-products-grid').innerHTML=productsHTML;
+//const addedMessageTimeouts={}; 
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const productId= button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantity();
+        });
+    });
+           
+            /*const addedMessage=document.querySelector(
                 `.js-added-to-cart-${productId}`
 
             );
@@ -113,3 +102,4 @@ document.querySelectorAll('.js-add-to-cart')
         });
 
     });
+*/
